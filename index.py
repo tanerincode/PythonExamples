7c2fbe311f1e63bd9212ec08b836d22e17aef724
Taner Tombas <tombastaner@gmail.com>
2018-04-27 15:19:53 +0300

    end example 1

--- index.py
+++ index.py
@@ -1,20 +1,51 @@
+import quandl, math
+import numpy as np
 import pandas as pd
-import quandl
-import math
+from sklearn import preprocessing, cross_validation, svm
+from sklearn.linear_model import LinearRegression
 
+df = quandl.get("WIKI/GOOGL")
 
-df = quandl.get('WIKI/GOOGL')
+print(df.head())
+#print(df.tail())
 
-df = df[['Adj. Open', 'Adj. High', 'Adj. Low', 'Adj. Close', 'Adj. Volume']]
-df['HL_PCT'] = (df['Adj. High'] - df['Adj. Close']) / df['Adj. Close'] * 100.0
-df['PTC_change'] = (df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open'] * 100.0
+df = df[['Adj. Open',  'Adj. High',  'Adj. Low',  'Adj. Close', 'Adj. Volume']]
 
-df = df[['Adj. Close', 'HL_PCT', 'PTC_change', 'Adj. Volume']]
+df['HL_PCT'] = (df['Adj. High'] - df['Adj. Low']) / df['Adj. Close'] * 100.0
+df['PCT_change'] = (df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open'] * 100.0
 
-forecast_col = 'Adj, Close'
+df = df[['Adj. Close', 'HL_PCT', 'PCT_change', 'Adj. Volume']]
+#print(df.head())
 
-df.fillna(-99999, inplace=True)
-
-forecast_out = int(math.ceil(0.1*len(df)))
+forecast_col = 'Adj. Close'
+df.fillna(value=-99999, inplace=True)
+forecast_out = int(math.ceil(0.01 * len(df)))
 
 df['label'] = df[forecast_col].shift(-forecast_out)
+df.dropna(inplace=True)
+
+X = np.array(df.drop(['label'], 1))
+y = np.array(df['label'])
+
+X = preprocessing.scale(X)
+y = np.array(df['label'])
+
+X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.2)
+
+clf = svm.SVR()
+
+clf.fit(X_train, y_train)
+
+confidence = clf.score(X_test, y_test)
+
+#print(confidence)
+
+clf = LinearRegression()
+
+clf = LinearRegression(n_jobs=-1)
+
+for k in ['linear', 'poly', 'rbf', 'sigmoid']:
+    clf = svm.SVR(kernel=k)
+    clf.fit(X_train, y_train)
+    confidence = clf.score(X_test, y_test)
+    print(k, confidence)
